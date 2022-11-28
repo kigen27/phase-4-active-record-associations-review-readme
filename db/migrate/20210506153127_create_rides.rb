bb5ff881929fff648768ceec0e4cf303be1cf372
Marley <andrew.kigen@student.moringaschool.com>
2022-11-28 10:13:00 +0300

    completed assignment

---
+++ db/migrate/20210506153127_create_rides.rb
@@ -0,0 +1,9 @@
+class CreateRides < ActiveRecord::Migration[6.1]
+    def change
+      create_table :rides do |t|
+        t.belongs_to :taxi
+        t.belongs_to :passenger
+        t.timestamps
+      end
+    end
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,52 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_151732) do
+ActiveRecord::Schema.define(version: 2021_05_06_153127) do
 
-  create_table "authors", force: :cascade do |t|
-    t.string "name"
+  create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "post_tags", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "tag_id", null: false
+  create_table "rides", force: :cascade do |t|
+    t.integer "taxi_id"
+    t.integer "passenger_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_post_tags_on_post_id"
-    t.index ["tag_id"], name: "index_post_tags_on_tag_id"
+    t.index ["passenger_id"], name: "index_rides_on_passenger_id"
+    t.index ["taxi_id"], name: "index_rides_on_taxi_id"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.integer "author_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_id"], name: "index_posts_on_author_id"
-  end
-
-  create_table "profiles", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "bio"
-    t.string "avatar_url"
-    t.string "facebook"
-    t.integer "author_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_id"], name: "index_profiles_on_author_id"
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
+  create_table "taxis", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "post_tags", "posts"
-  add_foreign_key "post_tags", "tags"
-  add_foreign_key "posts", "authors"
-  add_foreign_key "profiles", "authors"
 end
